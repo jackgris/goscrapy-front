b9fd42ed1558792d193f68070e60826ca753e85a
Gabriel Pozo <jackgris2@gmail.com>
2023-03-02 18:48:14 -0300

    update cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,8 +4,16 @@ export default defineConfig({
 	fileServerFolder: 'dist',
 	fixturesFolder: false,
 	projectId: '48i9ez',
+
 	e2e: {
 		baseUrl: 'http://localhost:4173/',
 		specPattern: 'cypress/e2e/**/*.ts'
+	},
+
+	component: {
+		devServer: {
+			framework: 'react',
+			bundler: 'vite'
+		}
 	}
 })

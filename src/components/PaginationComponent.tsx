50ad5f7eb5366fe9a7b736dbe447a68e87befbaa
Gabriel Pozo <jackgris2@gmail.com>
2023-03-16 20:37:28 -0300

    add: back and next buttons on pagination

--- src/components/PaginationComponent.tsx
+++ src/components/PaginationComponent.tsx
@@ -7,7 +7,9 @@ interface PaginationProperties {
 	onChangePage: (page: number) => void
 }
 
-const STYLE = 'p-5'
+const STYLE = 'space-x-2.5  mb-2 text-2xl font-bold tracking-tight'
+const STYLE_BUTTON =
+	'hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 active:text-black disabled:text-gray-900'
 
 const PaginationComponent: FC<PaginationProperties> = ({
 	currentPage,
@@ -24,12 +26,24 @@ const PaginationComponent: FC<PaginationProperties> = ({
 		{ length: totalPages },
 		(_, index) => index + add
 	)
+	const START = 0
+	const END = totalPages + add
 
 	return (
 		<div className={`${STYLE}`}>
+			<button
+				className={`${STYLE_BUTTON}`}
+				type='button'
+				key={START}
+				onClick={(): void => onChangePage(currentPage - add)}
+				disabled={currentPage === add}
+			>
+				Back
+			</button>
 			{pages.map(
 				(page: number): JSX.Element => (
 					<button
+						className={`${STYLE_BUTTON}`}
 						type='button'
 						key={page}
 						onClick={(): void => onChangePage(page)}
@@ -39,6 +53,15 @@ const PaginationComponent: FC<PaginationProperties> = ({
 					</button>
 				)
 			)}
+			<button
+				className={`${STYLE_BUTTON}`}
+				type='button'
+				key={END}
+				onClick={(): void => onChangePage(currentPage + add)}
+				disabled={currentPage === totalPages}
+			>
+				Next
+			</button>
 		</div>
 	)
 }
